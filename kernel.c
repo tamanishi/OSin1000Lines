e9fe8f17eb1521ee7dc870787ce4b39e9b048009
Masayuki Sunahara <tamanishi@gmail.com>
2025-01-11 17:54:45 +0900

    kernel panic

--- kernel.c
+++ kernel.c
@@ -30,6 +30,9 @@ void kernel_main(void) {
     printf("\n\nHello %s\n", "World!");
     printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
 
+    PANIC("booted!");
+    printf("unreachable here!\n");
+
     for (;;) {
         __asm__ __volatile__("wfi");
     }
